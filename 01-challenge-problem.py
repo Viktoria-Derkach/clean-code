f8448f1cab269a4da406070bdb9e9b2cc9b86d6c
ViktoriiaDerkach <viktoriia.derkach@airslate.com>
2023-04-09 16:45:35 +0300

    chalenge

--- 01-challenge-problem.py
+++ 01-challenge-problem.py
@@ -5,31 +5,28 @@ class Point:
 
 
 class Rectangle:
-    def __init__(self, starting_point, broad, high):
+    def __init__(self, starting_point, width, height):
         self.starting_point = starting_point
-        self.broad = broad
-        self.high = high
+        self.width = width
+        self.height = height
 
-    def area(self):
-        return self.broad * self.high
+    def generate_area(self):
+        return self.width * self.height
 
-    def end_points(self):
-        top_right = self.starting_point.coordX + self.broad
-        bottom_left = self.starting_point.coordY + self.high
+    def print_end_points(self):
+        top_right_coords = self.starting_point.coordX + self.width
+        bottom_left_coords = self.starting_point.coordY + self.height
         print('Starting Point (X)): ' + str(self.starting_point.coordX))
         print('Starting Point (Y)): ' + str(self.starting_point.coordY))
-        print('End Point X-Axis (Top Right): ' + str(top_right))
-        print('End Point Y-Axis (Bottom Left): ' + str(bottom_left))
+        print('End Point X-Axis (Top Right): ' + str(top_right_coords))
+        print('End Point Y-Axis (Bottom Left): ' + str(bottom_left_coords))
 
 
-def build_stuff():
-    main_point = Point(50, 100)
-    rect = Rectangle(main_point, 90, 10)
+def generate_rectangle():
+    starting_point = Point(50, 100)
+    return Rectangle(starting_point, 90, 10)
 
-    return rect
+generated_rectangle = generate_rectangle()
 
-
-my_rect = build_stuff()
-
-print(my_rect.area())
-my_rect.end_points()
+print(generated_rectangle.generate_area())
+generated_rectangle.print_end_points()
